a24d39fc45322ced5d486057a0b32aa618613715
Rudhra999 <50884865+Rudhra999@users.noreply.github.com>
2021-09-23 02:54:26 +0530

    A1

---
+++ php-docker/SQL.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: 
+-- Date: 2021-02-22 21:22
+*/
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (1,'2021001','Ankit Jaat','M','CSB');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (2,'2021002','Ankit Gujjar','M','CSB');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (3,'2021003','Ankit Pandit','M','CSB');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (4,'2021004','Harshita choudhary','F','CSE');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (5,'2021005','Ayush Srivastas','M','CSD');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (6,'2021006','Rekha M.','F','CSSS');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (7,'2021007','Lalit Jaiswal','M','CSAM');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (8,'2021008','Mayank Bharti','M','ECE');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (9,'2021009','Kanchan Choudhary','F','CSAI');
+INSERT INTO `` (`S.no.`,`Roll No.`,`Name`,`Gender`,`Branch`) VALUES (10,'2021010','Shreya Tomar','F','CSAM');
